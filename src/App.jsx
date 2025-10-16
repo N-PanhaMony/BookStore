5d825931c48a604c8be42022cde5e3fe2d78548e
N-PanhaMony <neangpanhamony001@gmail.com>
2025-10-16 22:58:21 +0700

    Update API URL for fetching books

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,78 @@
-import { useState } from "react"
-import Search from "./components/Search.jsx"
+import { useState, useEffect } from "react";
+import Search from "./components/Search.jsx";
 
-const App = () =>{
+const API_BASE_URL = "https://openlibrary.org";
 
-const [searchTerm , setSearchTerm] = useState("")
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [bookList, setBookList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  return(<main>
+  const fetchBooks = async () => {
+    setIsLoading(true);
+    setErrorMessage("");
 
-    <div className="pattern">
-      <div className="wrapper">
-        <header>
-          <img src='/public/book poster.jpg' alt=''/>
-          <h1>Find Your <span className="text-gradient">Favorites Books</span> Here</h1>
-        </header>
+    try {
+      const endpoint = `${API_BASE_URL}/search.json?author=tolkien&sort=new`;
+      const response = await fetch(endpoint);
+      if (!response.ok) throw new Error("Failed");
 
-        <Search searchTerm ={searchTerm} setSearchTerm={setSearchTerm}/>
-        <h1 className="text-gray-100">{searchTerm}</h1>
+      const data = await response.json();
+      console.log(data);
+
+      if (!data.docs || data.docs.length === 0) {
+        setErrorMessage("No books found");
+        setBookList([]);
+        return;
+      }
+
+      setBookList(data.docs);
+    } catch (error) {
+      console.error(`Error fetching books ${error}`);
+      setErrorMessage("Try again later...");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchBooks();
+  }, [searchTerm]);
+
+  return (
+    <main>
+      <div className="pattern">
+        <div className="wrapper">
+          <header>
+            <img src="/book poster.jpg" alt="" />
+            <h1>
+              Find Your <span className="text-gradient">Favorites Books</span> Here
+            </h1>
+            <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+          </header>
+
+          <section className="book-catalog">
+            <h1>All Books</h1>
+
+            {isLoading ? (
+              <p className="text-black">Loading...</p>
+            ) : errorMessage ? (
+              <p className="text-black">{errorMessage}</p>
+            ) : (
+              <ul>
+                {bookList.map((book) => (
+                  <li key={book.key} className="text-black">
+                    {book.title}
+                  </li>
+                ))}
+              </ul>
+            )}
+          </section>
+        </div>
       </div>
-    </div>
+    </main>
+  );
+};
 
-  </main>)
-}
-
-export default App
+export default App;
